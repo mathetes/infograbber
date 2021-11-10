f88e6a4425339113f5a7d8e50505f00b1974ec05
mathetes <nsk.roman@gmail.com>
2021-11-10 16:37:53 +0700

    Work thrue Layout

---
+++ components/taskList/index.js
@@ -0,0 +1,12 @@
+import TaskListItem from "../taskListItem";
+
+const TaskList = () => {
+  return (
+    <>
+      <h3>Список задач</h3>
+      <TaskListItem />
+    </>
+  );
+};
+
+export default TaskList;
---
+++ components/taskListItem/index.js
@@ -0,0 +1,5 @@
+const TaskListItem = () => {
+  return <h5>Пунккт первый</h5>;
+};
+
+export default TaskListItem;
