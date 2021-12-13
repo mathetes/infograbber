9a8bcf9f51a67f11a6ca6c4468bd55bc890f5870
mathetes <mathetes@gmail.com>
2021-12-13 17:47:07 +0700

    remove done!

--- components/TaskItem/index.js
+++ components/TaskItem/index.js
@@ -6,17 +6,6 @@ import { RiCloseCircleLine } from "react-icons/ri";
 
 function TaskItem ({todos, removeTodo}) {
   
- todos = [
-    {
-      id: 1,
-      title: "Привет, мир",
-    },
-    { id: 2, title: "Установка" },
-    { id: 3, title: "Работаем!" },
-    { id: 4, title: "Учимся!" },
-    { id: 5, title: "Треним!" },
-    { id: 6, title: "Всем пока!" },
-  ];
 
   return todos.map((todo, index) => (
     
--- components/TaskList/index.js
+++ components/TaskList/index.js
@@ -2,12 +2,25 @@ import React, { useState } from "react";
 import TaskItem from "../TaskItem";
 
 function TaskList() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+ 
+      {
+        id: 1,
+        title: "Привет, мир",
+      },
+      { id: 2, title: "Установка" },
+      { id: 3, title: "Работаем!" },
+      { id: 4, title: "Учимся!" },
+      { id: 5, title: "Треним!" },
+      { id: 6, title: "Всем пока!" },
+
+  ]);
 
   const removeTodo = (id) => {
     const removeArr = [...todos].filter((todo) => todo.id !== id);
     setTodos(removeArr);
   };
+  
 
   return <TaskItem todos={todos} removeTodo={removeTodo} />;
 }
